d76aa10719046718db723616c53cf45a37cc75e2
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2021-12-08 06:30:02 +0900

    fix

---
+++ main.js
@@ -0,0 +1,72 @@
+const overlay = new OverlayAPI({
+  extendData: true,
+  silentMode: false,
+  seperateLB: true,
+});
+
+const jobColor = (jobType) => {
+  const jobTypeColor = {
+    'dps': 'rgba(200, 3, 8, 0.3)',
+    'tank': 'rgba(41,112,243,0.3)',
+    'healer': 'rgba(107,240,86,0.3)',
+  }
+  if (jobType) {
+    return jobTypeColor[jobType]
+  }
+
+  return 'rgba(128,0,255,0.3)'
+}
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    time: "00:00",
+    encounter: "Encounter",
+    totalDps: 0,
+    combatData: [],
+  },
+  methods: {
+    update(data) {
+      this.combatData = data.extendData.combatant
+      this.time = data.extendData.encounter.duration
+      this.totalDps = data.extendData.encounter.dps
+    },
+  },
+  mounted() {
+    overlay.addListener('CombatData', (data) => {
+      this.update(data)
+      console.log('listener of `CombatData`', data)
+    })
+    overlay.addListener('ChangeZone', (data) => {
+      console.log('listener of `ChangeZone`', data)
+    });
+    overlay.startEvent();
+  },
+  beforeDestroy: function () {
+  },
+  computed: {
+    active() {
+      return this.combatData.length != 0
+    },
+    sortedCombatData() {
+      const maxDps = Math.max(...this.combatData.map(data => data.dps))
+      return this.combatData.sort((a, b) => {
+        if (a.dps < b.dps) {
+          return 1
+        }
+        if (a.dps > b.dps) {
+          return -1
+        }
+        return 0
+      }).map(data => {
+        const dpsRange = Math.trunc(data.dps / this.maxDps * 100)
+        data.background
+          = `linear-gradient(to right, rgba(0, 0, 0, 0), ${jobColor(data.jobType)} 5%, ${jobColor(data.jobType)} ${dpsRange - 5}%, rgba(0, 0, 0, 0) ${dpsRange}%, rgba(0, 0, 0, 0))`
+        return data
+      })
+    },
+    maxDps() {
+      return Math.max(...this.combatData.map(data => data.dps)) || 1
+    }
+  }
+})
